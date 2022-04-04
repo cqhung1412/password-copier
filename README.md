0264163d9b924c0a543d2654a5dd7bdf2ad5f31e
hungcq3 <hungcq3@vng.com.vn>
2022-04-05 04:58:03 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend
+
+## Installation guide
+1. Clone this repo
+2. Run ```npm install```
+3. Run ```npm start```
+4. Go to [localhost:3000](localhost:3000)
---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { Component, Fragment } from 'react';
+
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import './App.css';
+
+import logo from './logo.svg';
+import { Form, Image, Input, Typography, Button, notification } from 'antd';
+
+const { Title } = Typography
+
+class App extends Component {
+  state = {
+    prefix: '', postfix: ''
+  };
+
+  componentDidMount() {
+    const prefix = localStorage.getItem('prefix');
+    const postfix = localStorage.getItem('postfix');
+    this.setState({
+      prefix: prefix || '',
+      postfix: postfix || ''
+    });
+  }
+
+  onFinish = (values) => {
+    const { prefix, postfix } = values
+    navigator.clipboard.readText()
+      .then(text => `${prefix}${text}${postfix}`)
+      .then(password => navigator.clipboard.writeText(password))
+      .then(result => {
+        localStorage.setItem('prefix', prefix)
+        localStorage.setItem('postfix', postfix)
+        this.setState({
+          prefix: prefix || '',
+          postfix: postfix || ''
+        })
+      })
+      .then(result => notification.success({
+        message: 'Copy successfully 😀',
+        description: 'Prefix and postfix will be stored in local storage for later use.'
+      }))
+      .catch(error => {
+        notification.error({
+          message: 'Copy fail 😔',
+          description: error.message || 'Prefix and postfix will be stored in local storage for later use.'
+        })
+      })
+  }
+
+  render() {
+    const { prefix, postfix } = this.state
+    return (
+      <Fragment>
+        <div className='area'>
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div className='header'>
+            <Image className='App-logo' src={logo} width={50} preview={false} />
+            <Title level={1} className='header-title' >Copier</Title>
+          </div>
+          <div>
+            <Form
+              name='copier'
+              initialValues={{ prefix, postfix }}
+              onFinish={this.onFinish}
+            >
+              <Form.Item name='prefix'>
+                <Input placeholder='Prefix' />
+              </Form.Item>
+              <Form.Item name='postfix'>
+                <Input placeholder='Postfix' />
+              </Form.Item>
+              <Form.Item style={{ width: '100%' }}>
+                <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                  Get My Password
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+};
+
+export default App;
